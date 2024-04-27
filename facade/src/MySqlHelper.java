bfb95af6420f3a7100674b4a022b24e2ab54fb99
mirmahfuz99 <mirmahfuz10@gmail.com>
2024-04-27 12:07:12 +0600

    facade design pattern implemented

---
+++ facade/src/MySqlHelper.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+
+public class MySqlHelper {
+
+    public static Connection getMySqlDBConnection() {
+        //get mysql connection using connection parameters
+        return null;
+
+    }
+
+    public void generateMySqlPDFReport(String tableName, Connection con){
+        //get data from table and generate pdf report 
+     
+    }
+
+
+    public void generateMySqlHTMLReport(String tableName, Connection con){
+        //get data from table and generate htm report 
+    }
+}
