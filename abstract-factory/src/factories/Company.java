3c5d2e545fd5863e2a79af7cd11b12f82ea9c3e8
mirmahfuz99 <mirmahfuz10@gmail.com>
2024-06-15 11:12:57 +0600

    comment added to factories class

--- abstract-factory/src/factories/Company.java
+++ abstract-factory/src/factories/Company.java
@@ -3,6 +3,8 @@ package factories;
 import products.Gpu;
 import products.Monitor;
 
+//Abstract factory interface. Abstract Factory—an interface with a list of creation methods for all products.
+//These methods must return abstract product types represented by the interfaces we extracted previously Gpu and Monitor.
 public abstract class Company {
 
 
--- abstract-factory/src/factories/MsiManufacturer.java
+++ abstract-factory/src/factories/MsiManufacturer.java
@@ -6,6 +6,10 @@ import products.Monitor;
 import products.MsiGpu;
 import products.MsiMonitor;
 
+// Now, how about the product variants? For each variant of a product family, we create a separate factory
+// class based on the AbstractFactory interface. A factory is a class that returns products of a particular kind. 
+// For example the MsiManufacturer can only create MsiGpu and MsiMonitor.
+
 public class MsiManufacturer extends Company{
 
     @Override
